71d4fcabb8aaf4ed248f5f958fa675f05f0bfd40
farhanz17 <mufhan623@gmail.com>
2021-11-07 23:20:21 +0700

    tambahkan latihan01.py

---
+++ latihan01.py
@@ -0,0 +1,15 @@
+#program mencari nilai n lebih fari 0,5
+
+print(10*"waduch ")
+
+print(10*":)")
+import random
+jumlah = 5
+a = 0
+for x in range (jumlah):
+    i = random.uniform (.0,.5)
+    a+=1
+    print("data ke=" ,a,"===>",i)
+print("selesai")
+
+print(10*":)")
