200af849a97de13febf97ca591e56627c569121e
farhanz17 <mufhan623@gmail.com>
2021-11-11 21:43:07 +0700

    Farhan nambahin Latihan2.py

---
+++ Latihan2.py
@@ -0,0 +1,15 @@
+#Latihan_kedua_ya_bapak
+
+print("Masukkan bilangan terbesar antara 1-99")
+
+t = input("masukan bilangan 1 = ")
+u = input("masukan bilangan 2 = ")
+v = input("masukan bilangan 3 = ")
+w = input("masukan bilangan 4 = ")
+x = input("masukan bilangan 5 = ")
+y = input("masukan bilangan 6 = ")
+z = input("masukan bilangan 7 = ")
+
+data = [t,u,v,w,x,y,z]
+
+print("Bilangan terbasar adalah : ", max(data))
